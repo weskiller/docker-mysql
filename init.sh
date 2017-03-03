5b7c6855c856a7f97502d0b2b7c9918424bfef7b
weskiller <weskiller@vip.qq.com>
2017-03-03 12:24:53 +0800

    debug

--- init.sh
+++ init.sh
@@ -3,7 +3,7 @@ exec >&2
 path_socket=/var/lib/mysql/mysql.sock
 if [ -f /etc/my.cnf ];then
 	mysql_config="/etc/my.cnf"
-	path_socket=$(grep -i socker $mysql_config|awk '{print $2}')
+	path_socket=$(grep -i -P "^\S*socket" /etc/my.cnf|awk -F= 'END{print $2}')
 else
 	echo "echo /etc/my.cnf not define!"
 	exit 1
